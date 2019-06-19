cb0448fefadd6d8f555febe5d25a8d70d9bd2297
Jenny Karlsson <jennyfz.karlsson@gmail.com>
2019-06-19 15:55:00 +0200

    added some code in python file

---
+++ testIt.py
@@ -0,0 +1,28 @@
+#this is a comment
+
+print('hello world!')
+
+number = 1
+number2 = 2
+
+#code below will just add to numbers
+
+print(number + number2)
+
+#let the user write something to the console
+num1 = input("Enter")
+
+num2 = input("Enter next number")
+
+sum = num1 + num2
+print(sum)
+
+
+
+#function that will print the area of a triangle
+
+base = float(input("Please enter base: "))
+
+height = input("Please enter te height: ")
+
+area =  ()
